bbb6ba2a94ece967e01d7b58a303d392e3f4e295
Attar <93458132+att-ar@users.noreply.github.com>
2022-08-27 23:30:11 -0400

    Update transformer_helper.py
    making mask output 2-D

--- transformer_helper.py
+++ transformer_helper.py
@@ -50,28 +50,6 @@ def positional_encoding(positions: int, d: int):
     # casts tensor to float dtype
     return tf.cast(pos_encoding, dtype=tf.float32)
 
-#####
-# Two types of masks that are useful when building your Transformer network: the *padding mask* and the *look-ahead mask*
-#####
-
-
-def create_padding_mask(decoder_token_ids):
-    """
-    Creates a matrix mask for the padding cells
-    Still need to replace zeros with negative infinities to not cause problems when applying softmax
-
-    Arguments:
-        decoder_token_ids -- (n, m) matrix
-
-    Returns:
-        mask -- (n, 1, m) binary tensor
-    """
-    seq = 1 - tf.cast(tf.math.equal(decoder_token_ids, 0), tf.float32)
-    # tf.math.equal(x,y) Returns the truth value of (x == y) element-wise.
-    # True has numerical value 1, False 0 so all the zeros turn into 1 here, and everything else becomes 0
-    
-    return seq
-
 
 def create_look_ahead_mask(sequence_length):
     """
@@ -86,13 +64,13 @@ def create_look_ahead_mask(sequence_length):
         mask -- (size, size) tensor
 
     >>>create_look_ahead_mask(5)
-    <tf.Tensor: shape=(1, 5, 5), dtype=float32, numpy=
-    array([[[1., 0., 0., 0., 0.],
-            [1., 1., 0., 0., 0.],
-            [1., 1., 1., 0., 0.],
-            [1., 1., 1., 1., 0.],
-            [1., 1., 1., 1., 1.]]], dtype=float32)>
+    <tf.Tensor: shape=(5, 5), dtype=float32, numpy=
+    array([[1., 0., 0., 0., 0.],
+           [1., 1., 0., 0., 0.],
+           [1., 1., 1., 0., 0.],
+           [1., 1., 1., 1., 0.],
+           [1., 1., 1., 1., 1.]], dtype=float32)>
     """
     mask = tf.linalg.band_part(
         tf.ones((1, sequence_length, sequence_length)), -1, 0)
-    return mask
+    return mask.squeeze()
